dcf1e9c8953dd88014b79c25d9679b862032684a
MARMOT <63745881+Swistek92@users.noreply.github.com>
2024-06-30 21:15:33 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,198 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface empInferface {
+  id: number;
+  name: string;
+  role: string;
+}
+type ColorMode = 'lightMode' | 'darkMode';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Demo';
+  constructor() {
+    console.log('calling constructor');
+  }
+  ngOnInit(): void {
+    console.log('calling on init');
+
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
+
+  // name = '';
+  // email = '';
+  // phone = '';
+  // address = '';
+  // qualifications = [{ school: '', degree: '', year: '' }];
+  // submitted: boolean = false;
+  // addQualification() {
+  //   this.qualifications.push({ school: '', degree: '', year: '' });
+  // }
+  // submit() {
+  //   this.submitted = true;
+  // }
+  // edit() {
+  //   this.submitted = false;
+  // }
+  // employers20 = [
+  //   { id: 1, name: 'jakub', salar: 2000 },
+  //   { id: 2, name: 'marian', salar: 4000 },
+  //   { id: 3, name: 'stefan', salar: 6000 },
+  // ];
+  // isAuth: boolean = false;
+  // nm: string = '';
+  // em: string = '';
+  // emailIsValid: boolean = false;
+  // formSubmitted: boolean = false;
+  // showMessage() {
+  //   if (this.nm.length > 5 && this.emailIsValid) {
+  //     this.formSubmitted = true;
+  //   } else {
+  //     this.formSubmitted = false;
+  //   }
+  // }
+  // emailValidation(e: string): void {
+  //   console.log(e);
+  //   if (e.includes('@')) {
+  //     this.emailIsValid = true;
+  //   } else {
+  //     this.emailIsValid = false;
+  //   }
+  // }
+  // applyVal = { color: 'green', 'font-size': '40px' };
+  // colorMode: ColorMode = 'lightMode';
+  // toggleMode() {
+  //   if (this.colorMode === 'lightMode') {
+  //     this.colorMode = 'darkMode';
+  //   } else {
+  //     this.colorMode = 'lightMode';
+  //   }
+  // }
+  // hasText: boolean = false;
+  // textInput(e: any) {
+  //   if (e.target.value !== '') {
+  //     this.hasText = true;
+  //   } else {
+  //     this.hasText = false;
+  //   }
+  // }
+  // // updateStyles= 'updateStyles'
+  // age1: number = 0;
+  // isEligible: boolean = false;
+  // checkAge() {
+  //   // if(this.age1> 18){
+  //   //   this.isEligible = true
+  //   // }else{
+  //   //   this.isEligible = false
+  //   // }
+  //   this.isEligible = this.age1 >= 18;
+  // }
+  // age = 10;
+  // employeeRole: string = '';
+  // employees = signal<empInferface[]>([
+  //   {
+  //     id: 1,
+  //     name: 'adam',
+  //     role: 'dev',
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'React',
+  //     role: 'dev',
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Node',
+  //     role: 'dev',
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'MongoDb',
+  //     role: 'marketing',
+  //   },
+  //   {
+  //     id: 5,
+  //     name: 'adam',
+  //     role: 'marketing',
+  //   },
+  //   {
+  //     id: 6,
+  //     name: 'React',
+  //     role: 'marketing',
+  //   },
+  //   {
+  //     id: 7,
+  //     name: 'Node',
+  //     role: 'analyst',
+  //   },
+  //   {
+  //     id: 8,
+  //     name: 'MongoDb',
+  //     role: 'analyst',
+  //   },
+  // ]);
+  // title = 'Demo';
+  // counter = 0;
+  // staticInput: string = '2 way banding';
+  // dynamicInput: string = 'dynamic input';
+  // show: boolean = false;
+  // fullstackDev = [
+  //   {
+  //     id: 1,
+  //     name: 'Angular',
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'React',
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Node',
+  //   },
+  //   {
+  //     id: 1,
+  //     name: 'MongoDb',
+  //   },
+  // ];
+  // list = [
+  //   {
+  //     name: 'alex',
+  //     age: 24,
+  //   },
+  //   {
+  //     name: 'alex1',
+  //     age: 31,
+  //   },
+  //   {
+  //     name: 'alex2',
+  //     age: 52,
+  //   },
+  //   {
+  //     name: 'aaaa',
+  //     age: 73,
+  //   },
+  //   {
+  //     name: 'alex6',
+  //     age: 16,
+  //   },
+  // ];
+  // display(msg: string) {
+  //   alert(msg)
+  // }
+  // incr(){
+  //   this.counter++
+  // }
+  // keypressed(e:any){
+  //   console.log(e.target.value)
+  // }
+  // isShift(e:KeyboardEvent){
+  //   console.log(e)
+  //     if(e.shiftKey && e.key === "Y"){
+  //       console.log('shist + Y is pressed')
+  //     }
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { FormsModule } from '@angular/forms';
+
+// ng new Demo --no-standalone
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+
   ],
   providers: [
     provideClientHydration()
